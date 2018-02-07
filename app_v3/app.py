b66abae1e56e3795016c99e0d8bc2f3ef0d46804
onifinger <ryuzi.toy@gmail.com>
2018-02-07 05:59:25 +0000

    add css & change UI

--- app_v3/app.py
+++ app_v3/app.py
@@ -16,10 +16,10 @@ from gcloud import storage
 import uuid, tempfile
 
 #### Edit Here
-project_id = '<Your_Project_ID>'
+project_id = 'onifindevopslab'
 dbuser = 'appuser'
-dbpass = 'pas4appuser'
-dbinstance = '<Your_Instance_Connection_Name>'
+dbpass = 'pass4user'
+dbinstance = 'onifindevopslab:asia-northeast1:websql'
 ####
 
 app = Flask(__name__)
